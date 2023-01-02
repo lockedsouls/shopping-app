3577d42f84cbcbcb0a209b4eefaf3c1455ee7d04
lockedsouls <lockedssouls@gmail.com>
2023-01-03 00:54:57 +0200

    modify(public/loginPage/loginPage.html) include javascript script | modify(public/loginPage/loginPage.js) add onclick events for button and 'create account'

---
+++ public/loginPage/loginPage.js
@@ -0,0 +1,7 @@
+document.querySelector("#authenticate > input").onclick = () => {
+    //api call to backend
+};
+
+document.querySelector("#container > span > span").onclick = () => {
+    console.log("redirect to register page");
+};
