976d14ad20a0f62a4ea5635c40d9d3a209d885e8
MTL-CristianBlanaru <cristian.blanaru@mindthevalue.com>
2023-01-13 16:28:03 +0200

    modify(src/loginPage.ts) optimize the code

--- dist/loginPage.js
+++ dist/loginPage.js
@@ -1,15 +1,13 @@
 "use strict";
-const password_field = document.querySelector("#password > input");
-const toggle_password_visiblity = document.querySelector("#password > img");
-if (toggle_password_visiblity) {
-    toggle_password_visiblity.onclick = (event) => {
-        if (password_field.type === "text") {
-            password_field.type = "password";
-            toggle_password_visiblity.src = "./img/icons/eye-slash-regular.svg";
-        }
-        else {
-            password_field.type = "text";
-            toggle_password_visiblity.src = "./img/icons/eye-regular.svg";
-        }
-    };
-}
+document.querySelector("#password > img").onclick = (event) => {
+    const target = event.target;
+    const password_field = document.querySelector("#password > input");
+    if (password_field.type === "text") {
+        password_field.type = "password";
+        target.src = "./img/icons/eye-slash-regular.svg";
+    }
+    else {
+        password_field.type = "text";
+        target.src = "./img/icons/eye-regular.svg";
+    }
+};
--- src/loginPage.ts
+++ src/loginPage.ts
@@ -1,14 +1,12 @@
-const password_field = document.querySelector("#password > input") as HTMLInputElement;
-const toggle_password_visiblity = document.querySelector("#password > img") as HTMLImageElement;
+(document.querySelector("#password > img") as HTMLImageElement).onclick = (event) => {
+    const target = event.target as HTMLImageElement;
+    const password_field = document.querySelector("#password > input") as HTMLInputElement;
 
-if (toggle_password_visiblity){
-    toggle_password_visiblity.onclick = (event) => {
-        if (password_field.type === "text"){
-            password_field.type = "password";
-            toggle_password_visiblity.src = "./img/icons/eye-slash-regular.svg";
-        }else{
-            password_field.type = "text";
-            toggle_password_visiblity.src = "./img/icons/eye-regular.svg";
-        }
+    if (password_field.type === "text"){
+        password_field.type = "password";
+        target.src = "./img/icons/eye-slash-regular.svg";
+    }else{
+        password_field.type = "text";
+        target.src = "./img/icons/eye-regular.svg";
     }
 }
