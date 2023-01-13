263c75f16270c1eeda14fa2eea869a08cc2290bf
lockedsouls <lockedssouls@gmail.com>
2023-01-14 00:19:36 +0200

    modify(loinPage/[loginPage.html; loginPage.css]) remove excessive div

---
+++ dist/lib/test.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class LOL {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+}
+exports.default = LOL;
