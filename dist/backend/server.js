56d431bdaf35b9bf7f76b3a7ed54f40b18e8334f
MTL-CristianBlanaru <cristian.blanaru@mindthevalue.com>
2023-01-16 17:16:22 +0200

    fix(*) fix tsconfig

--- dist/backend/server.js
+++ dist/backend/server.js
@@ -5,4 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+console.log("hey");
 app.listen(3000, () => { console.log("Server running..."); });
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
 const app = express();
 
+console.log("hey");
+
 app.listen(3000, () => {console.log("Server running...")});
